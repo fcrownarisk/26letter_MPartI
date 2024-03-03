e1ce304ce66a971bd3b84e5f1f888048a540ee58
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-03 03:10:50 -0800

    Delete fenymann&diagram directory

--- fenymann&diagram/I4.ts
+++
@@ -1,8 +0,0 @@
-import '../string/Momentum'
-import '../fenymann diagram/fenymann diagram'
-import '../scatter.css'
-export function momentum(){
-    const I4=
-    'p1'+'p2'=='p3'+'p4'
-    return 'scatter.css'
-}
--- fenymann&diagram/diagram.ts
+++
@@ -1,9 +0,0 @@
-import '../particle/boson'
-import '../particle/fermion'
-import '../geometry/topology/ChenClass'
-import '../geometry/topology/LieGroup'
-import '../geometry/topology/manifold'
-import '../geometry/topology/orbifold'
-
-
-console.log(`Diagram`,`vertices`,`edges`)
