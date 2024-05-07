6351d167ab5fa15ffa7f9fa0b59a33f52ff42bb7
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-07 18:36:25 +0800

    Update diagram.ts

--- fenymann&diagram/diagram.ts
+++ fenymann&diagram/diagram.ts
@@ -2,8 +2,4 @@ import '../particle/boson'
 import '../particle/fermion'
 import '../geometry/topology/ChenClass'
 import '../geometry/topology/LieGroup'
-import '../geometry/topology/manifold'
-import '../geometry/topology/orbifold'
-
-
 console.log(`Diagram`,`vertices`,`edges`)
